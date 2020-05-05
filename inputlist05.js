2ba4487e75fb7da7b67b00295c196d1de9bae3e2
Will Brown <wtbrown10@gmail.com>
2020-05-04 22:25:06 -0400

    add for loop, if statement, alert

--- inputlist05.js
+++ inputlist05.js
@@ -4,5 +4,20 @@ function addNameToArray() {
   let nameToAdd = document.getElementById("inputBox").value;
   arrayNames.push(nameToAdd);
 
-  localStorage.setItem("lsArryNames", JSON.stringify(arrayNames));
+  if (nameToAdd.length != 0) {
+    localStorage.setItem("lsArryNames", JSON.stringify(arrayNames));
+
+    let parsedArray = JSON.parse(localStorage.getItem("lsArrayNames"));
+
+    let htmlText = "<ul>";
+
+    for (let i = 0; i < arrayNames.length; i++) {
+      htmlText = htmlText + `<li>${arrayNames[i]}</li>`;
+    }
+
+    htmlText = htmlText + "</ul>";
+    document.getElementById("theList").innerHTML = htmlText;
+  } else {
+    alert("Please enter Name!");
+  }
 }
