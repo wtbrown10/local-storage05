9a8b92cebd675d25ed9731c43e35f5f8af8c4839
Will Brown <wtbrown10@gmail.com>
2020-05-19 22:23:26 -0400

    add div, header, h1

---
+++ domjs.js
@@ -0,0 +1,9 @@
+let header = document.createElement("h1");
+
+// document.body.appendChild(header).innerText = "Hello";
+
+let div1 = document.querySelector("div");
+
+div1.appendChild(header).innerText = "Hello";
+
+document.createElement("div");
